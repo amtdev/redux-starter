5f7a0a94fcaf9e15f335b0dc6cf1edb3dc878d81
AMTdev Alien <craig@amtdev.com>
2024-04-18 20:38:42 -0400

    implemented add to cart handler

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,29 +1,41 @@
-import { BaggageClaim } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+'use client';
+import { addToCart } from '@/redux/slices/cartSlice';
+import { BaggageClaim } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Product({ product }) {
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    console.log(product);
+    dispatch(addToCart(product));
+  }
   return (
-    <div className="rounded-lg mr-3  bg-white dark:bg-slate-900 overflow-hidden border shadow">
+    <div className="mr-3 overflow-hidden bg-white border rounded-lg shadow dark:bg-slate-900">
       <Link href="#">
         <Image
           src={product.image}
           alt={product.title}
           width={556}
           height={556}
-          className="w-full h-48 object-cover"
+          className="object-cover w-full h-48"
         />
       </Link>
       <div className="px-4">
         <Link href={`/products/${product.slug}`}>
-          <h2 className="text-center dark:text-slate-200 text-slate-800 my-2 font-semibold">
+          <h2 className="my-2 font-semibold text-center dark:text-slate-200 text-slate-800">
             {product.title}
           </h2>
         </Link>
         <div className="flex items-center justify-between gap-2 pb-3 dark:text-slate-200 text-slate-800">
           <p>$ {product.price}</p>
-          <button className="flex items-center space-x-2 bg-lime-600 px-4 py-2 rounded-md text-white">
+          <button
+            onClick={() => handleAddToCart()}
+            className="flex items-center px-4 py-2 space-x-2 text-white rounded-md bg-lime-600"
+          >
             <BaggageClaim />
             <span>Add</span>
           </button>
